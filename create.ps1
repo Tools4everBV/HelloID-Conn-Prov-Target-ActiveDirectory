21b546da681df9f257ad87e9831e96973658ac5f
Mike Sheldon <m.sheldon@tools4ever.com>
2021-03-02 10:50:53 -0800

    Documenation and Improvements

--- create.ps1
+++ create.ps1
@@ -1,20 +1,36 @@
-#Initialize default properties
-$c = $configuration | ConvertFrom-Json
+#region Initialize default properties
+$config = ConvertFrom-Json $configuration
 $p = $person | ConvertFrom-Json
 $pp = $previousPerson | ConvertFrom-Json
 $pd = $personDifferences | ConvertFrom-Json
 $m = $manager | ConvertFrom-Json
 
-$success = $False;
+$success = $False
 $auditLogs = New-Object Collections.Generic.List[PSCustomObject];
 
 #Get Primary Domain Controller
 $pdc = (Get-ADForest | Select-Object -ExpandProperty RootDomain | Get-ADDomain | Select-Object -Property PDCEmulator).PDCEmulator
 
+#endregion Initialize default properties
+
+#region Change mapping here
+    #Correlation
+    $correlationPersonField = ($config.correlationPersonField | Invoke-Expression)
+    $correlationAccountField = $config.correlationAccountField
+#endregion Change mapping here
+
+#region Execute
 try{
     #Find AD ACcount by employeeID attribute
-	$account = Get-ADUser -LdapFilter "(employeeId=$($p.externalId))" -Property sAMAccountName -Server $pdc
+    $filter = "($($correlationAccountField)=$($correlationPersonField))";
+    Write-Information "LDAP Filter: $($filter)";
+    
+	$account = Get-ADUser -LdapFilter $filter -Property sAMAccountName -Server $pdc
 	
+    if($account -eq $null) { throw "Failed to return a account" }
+
+    Write-Information "Account correlated to $($account.sAMAccountName)";
+
 	$auditLogs.Add([PSCustomObject]@{
                 Action = "CreateAccount"
                 Message = "Account correlated to $($account.sAMAccountName)";
@@ -25,15 +41,16 @@ try{
 }
 catch
 {
-    auditLogs.Add([PSCustomObject]@{
+    $auditLogs.Add([PSCustomObject]@{
                 Action = "CreateAccount"
                 Message = "Account failed to correlate:  $_"
                 IsError = $True
             });
 	Write-Error $_;
 }
+#endregion Execute
 
-#build up result
+#regionbuild up result
 $result = [PSCustomObject]@{
     Success= $success;
     AccountReference= $account.SID.Value
@@ -41,5 +58,5 @@ $result = [PSCustomObject]@{
     Account = $account;
 };
 
-#send result back
 Write-Output $result | ConvertTo-Json -Depth 10
+#endregion build up result
