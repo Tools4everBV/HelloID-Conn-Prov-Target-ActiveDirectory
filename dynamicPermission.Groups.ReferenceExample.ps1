62b67c4ffbb4bd8b27f3450a66e84951417d3146
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2022-03-29 14:22:01 +0200

    Updated to use groupguid to prevent naming issues
    Updated to include a funciton to sanitize AD group name and use the group guid as identifier to prevent any issues with renaming a group.

--- dynamicPermission.Groups.ReferenceExample.ps1
+++ dynamicPermission.Groups.ReferenceExample.ps1
@@ -1,5 +1,3 @@
-# 2021-02-05 - Student Groups - Dynamic Permissions Example
-# 2021-08-17 - Updated with Entitlement Context variable change.
 #region Initialize default properties
 $config = ConvertFrom-Json $configuration
 $p = $person | ConvertFrom-Json
@@ -8,125 +6,143 @@ $pd = $personDifferences | ConvertFrom-Json
 $m = $manager | ConvertFrom-Json
 $aRef = $accountReference | ConvertFrom-Json
 $mRef = $managerAccountReference | ConvertFrom-Json
+$pRef = $entitlementContext | ConvertFrom-json
 
+$success = $True
+$auditLogs = New-Object Collections.Generic.List[PSCustomObject];
+$dynamicPermissions = New-Object Collections.Generic.List[PSCustomObject];
+
+$pdc = (Get-ADForest | Select-Object -ExpandProperty RootDomain | Get-ADDomain | Select-Object -Property PDCEmulator).PDCEmulator
+
+#region Supporting Functions
+function Get-ADSanitizeGroupName
+{
+    param(
+        [parameter(Mandatory = $true)][String]$Name
+    )
+    $newName = $name.trim();
+    # $newName = $newName -replace ' - ','_'
+    $newName = $newName -replace '[`,~,!,#,$,%,^,&,*,(,),+,=,<,>,?,/,'',",;,:,\,|,},{,.]',''
+    $newName = $newName -replace '\[','';
+    $newName = $newName -replace ']','';
+    # $newName = $newName -replace ' ','_';
+    $newName = $newName -replace '\.\.\.\.\.','.';
+    $newName = $newName -replace '\.\.\.\.','.';
+    $newName = $newName -replace '\.\.\.','.';
+    $newName = $newName -replace '\.\.','.';
+    return $newName;
+}
+#endregion Supporting Functions
+
+
+#region Change mapping here
+$desiredPermissions = @{};
+foreach ($contract in $p.Contracts) {
+    if (( $contract.Context.InConditions) ) {
+        $name = $contract.Department.ExternalId + ' - ' + $contract.Title.Name
+        $name = Get-ADSanitizeGroupName -Name $name
+
+        $ADGroup = $null
+        $ADGroup = Get-ADGroup -Filter { Name -eq $name } -Properties DisplayName
+        if ($null -eq $ADGroup) {
+            throw "No Group found with name: $name"
+        }
+        elseif ($ADGroup.sAMAccountName.count -gt 1) {
+            throw "Multiple Groups found with name: $name . Please correct this so the description is unique."
+        }
+
+        $group_DisplayName = $ADGroup.DisplayName
+        $group_ObjectGUID = $ADGroup.ObjectGUID
+        $desiredPermissions["$($group_DisplayName)"] = $group_ObjectGUID
+    }
+}
+
+Write-Verbose -Verbose ("Desired Permissions: {0}" -f ($desiredPermissions.keys | ConvertTo-Json))
+#endregion Change mapping here
+
+#region Execute
 # Operation is a script parameter which contains the action HelloID wants to perform for this permission
 # It has one of the following values: "grant", "revoke", "update"
 $o = $operation | ConvertFrom-Json
 
-# The entitlementContext contains the sub permissions (Previously the $permissionReference variable)
-$eRef = $entitlementContext | ConvertFrom-Json
+if ($dryRun -eq $True) {
+    # Operation is empty for preview (dry run) mode, that's why we set it here.
+    $o = "grant"
+}
 
+Write-Verbose -Verbose ("Existing Permissions: {0}" -f $entitlementContext)
 $currentPermissions = @{}
-foreach($permission in $eRef.CurrentPermissions) {
+foreach ($permission in $pRef.CurrentPermissions) {
     $currentPermissions[$permission.Reference.Id] = $permission.DisplayName
 }
 
-$success = $True
-$auditLogs = New-Object Collections.Generic.List[PSCustomObject]
-$subPermissions = New-Object Collections.Generic.List[PSCustomObject]
-
-$pdc = (Get-ADForest | Select-Object -ExpandProperty RootDomain | Get-ADDomain | Select-Object -Property PDCEmulator).PDCEmulator
-#endregion Initialize default properties
-
-#region Change mapping here
-$desiredPermissions = @{}
-if (-Not($o -eq "revoke"))
-{
-    foreach($contract in $p.Contracts) {  # If Testing:  -OR ($dryRun -eq $True)
-        if($contract.Context.InConditions)
-        {
-            # <GradYear>.Students.<Location>
-            $group_sAMAccountName = "{0}.Students.{1}" -f $p.custom.GradYear,$contract.Department.ExternalID
-            $desiredPermissions[$group_sAMAccountName] = $group_sAMAccountName
-
-            # Students.<Location>
-            $group_sAMAccountName = "Students.{0}" -f $contract.Department.ExternalID
-            $desiredPermissions[$group_sAMAccountName] = $group_sAMAccountName
-
-            # Students.<Alt Location>
-            if(-Not [string]::IsNullOrWhiteSpace($p.custom.AltLocation))
-            {
-                $group_sAMAccountName = "Students.{0}" -f $p.custom.AltLocation
-                $desiredPermissions[$group_sAMAccountName] = $group_sAMAccountName
-            }
-        }
-    }
-}
-Write-Verbose -Verbose ("Defined Permissions: {0}" -f ($desiredPermissions.keys | ConvertTo-Json))
-#endregion Change mapping here
-
-#region Execute
-Write-Verbose -Verbose ("Existing Permissions: {0}" -f $entitlementContext)
-
 # Compare desired with current permissions and grant permissions
-foreach($permission in $desiredPermissions.GetEnumerator()) {
-    $subPermissions.Add([PSCustomObject]@{
-            DisplayName = $permission.Value
-            Reference = [PSCustomObject]@{ Id = $permission.Name }
-    })
+foreach ($permission in $desiredPermissions.GetEnumerator()) {
+    $dynamicPermissions.Add([PSCustomObject]@{
+            DisplayName = $permission.Name
+            Reference   = [PSCustomObject]@{ Id = $permission.Value }
+        })
 
-    if(-Not $currentPermissions.ContainsKey($permission.Name))
-    {
+    if (-Not $currentPermissions.ContainsKey($permission.Value)) {
         # Add user to Membership
         $permissionSuccess = $true
-        if(-Not($dryRun -eq $True))
-        {
-            try
-            {
+        if (-Not($dryRun -eq $True)) {
+            try {
+                $ObjectGUID = "$($permission.Value)"
+                $ADGroup = Get-ADGroup -Identity $ObjectGUID
+
                 #Note:  No errors thrown if user is already a member.
-                Add-ADGroupMember -Identity $($permission.Name) -Members @($aRef) -server $pdc -ErrorAction 'Stop'
-                Write-Verbose -Verbose ("Successfully Granted Permission to: {0}" -f $permission.Name)
+                Add-ADGroupMember -Identity $ADGroup -Members @($aRef) -server $pdc
             }
-            catch
-            {
+            catch {
                 $permissionSuccess = $False
                 $success = $False
                 # Log error for further analysis.  Contact Tools4ever Support to further troubleshoot
-                Write-Error ("Error Granting Permission for Group [{0}]:  {1}" -f $permission.Name, $_)
+                Write-Warning ("Error Granting Permission for Group [{0}]:  {1}" -f "$($permission.Name), $($permission.Value)", $_)
             }
         }
 
         $auditLogs.Add([PSCustomObject]@{
-            Action = "GrantDynamicPermission"
-            Message = "Granted membership: {0}" -f $permission.Name
-            IsError = -NOT $permissionSuccess
-        })
+                Action  = "GrantDynamicPermission"
+                Message = "Granted membership: {0}" -f "$($permission.Name), $($permission.Value)"
+                IsError = -NOT $permissionSuccess
+            })
     }    
 }
 
 # Compare current with desired permissions and revoke permissions
 $newCurrentPermissions = @{}
-foreach($permission in $currentPermissions.GetEnumerator()) {    
-    if(-Not $desiredPermissions.ContainsKey($permission.Name) -AND $permission.Name -ne "No Groups Defined")
-    {
+foreach ($permission in $currentPermissions.GetEnumerator()) {    
+    if (-Not $desiredPermissions.ContainsKey($permission.Value)) {
         # Revoke Membership
-        if(-Not($dryRun -eq $True))
-        {
+        if (-Not($dryRun -eq $True)) {
             $permissionSuccess = $True
-            try
-            {
-                Remove-ADGroupMember -Identity $permission.Name -Members @($aRef) -Confirm:$false -server $pdc -ErrorAction 'Stop'
+            try {
+                $ObjectGUID = "$($permission.Value)"
+                $ADGroup = Get-ADGroup -Identity $ObjectGUID
+
+                Remove-ADGroupMember -Identity $ADGroup -Members @($aRef) -Confirm:$false -server $pdc
             }
             # Handle issue of AD Account or Group having been deleted.  Handle gracefully.
-            catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]{
+            catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
                 Write-Verbose -Verbose "Identity Not Found.  Continuing"
                 Write-Verbose -Verbose $_
             }
-            catch
-            {
+            catch {
                 $permissionSuccess = $False
                 $success = $False
                 # Log error for further analysis.  Contact Tools4ever Support to further troubleshoot.
-                Write-Error ("Error Revoking Permission from Group [{0}]:  {1}" -f $permission.Name, $_)
+                Write-Warning ("Error Revoking Permission from Group [{0}]:  {1}" -f "$($permission.Name), $($permission.Value)", $_)
             }
         }
         
         $auditLogs.Add([PSCustomObject]@{
-            Action = "RevokeDynamicPermission"
-            Message = "Revoked membership: {0}" -f $permission.Name
-            IsError = -Not $permissionSuccess
-        })
-    } else {
+                Action  = "RevokeDynamicPermission"
+                Message = "Revoked membership: {0}" -f "$($permission.Name), $($permission.Value)"
+                IsError = -Not $permissionSuccess
+            })
+    }
+    else {
         $newCurrentPermissions[$permission.Name] = $permission.Value
     }
 }
@@ -143,23 +159,13 @@ if ($o -eq "update") {
     }
 }
 #>
-
-# Handle case of empty defined dynamic permissions.  Without this the entitlement will error.
-if ($o -match "update|grant" -AND $subPermissions.count -eq 0)
-{
-    $subPermissions.Add([PSCustomObject]@{
-            DisplayName = "No Groups Defined"
-            Reference = [PSCustomObject]@{ Id = "No Groups Defined" }
-    })
-}
-
 #endregion Execute
 
 #region Build up result
 $result = [PSCustomObject]@{
-    Success = $success
-    SubPermissions = $subPermissions
-    AuditLogs = $auditLogs
-}
-Write-Output ($result | ConvertTo-Json -Depth 10)
+    Success            = $success;
+    DynamicPermissions = $dynamicPermissions;
+    AuditLogs          = $auditLogs;
+};
+Write-Output $result | ConvertTo-Json -Depth 10;
 #endregion Build up result
