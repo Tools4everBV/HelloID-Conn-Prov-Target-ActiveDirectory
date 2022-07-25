1389a50368d4dd753ad5e65a8d9378ca0ac1fa8f
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2022-07-25 21:15:49 +0200

    Delete dynamicPermission.ExchangeOnline.EnableRemoteMailbox(and Archive).ps1

--- dynamicPermission.ExchangeOnline.EnableRemoteMailbox(and Archive).ps1
+++
@@ -1,2 +0,0 @@
-These scripts have been moved to a new repository specific for NTFS actions.
-Please see the new repository for the script: https://github.com/Tools4everBV/HelloID-Conn-Prov-Target-ExchangeOnline
